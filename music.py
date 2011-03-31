6b0c78ad12ebb478e33633977e7c5eb1134d2230
gardn227 <gardn227@JARVIS.(none)>
2011-03-31 18:38:22 -0400

    Starting on organization, structure, logic, and recursive directory methods

--- music.py
+++ music.py
@@ -4,11 +4,42 @@ import os
 import sys
 import glob
 
-directory = '/home/gardn227/MB/'
+directory = '/home/gardn227/MB2/'
 match = '*'
+Filelist = glob.glob(directory+match)
 
-List = glob.glob(directory+match)
+def ffmpeg(originalfile, newfile):
+    '''
+    this function allows us to run the unix tool ffmpeg with our music files.
+    Takes two arguments, the first being the original filename and the second
+    being the filename to which you would like to save the result.
+    '''
+    command = 'ffmpeg -i "' + originalfile + '" "' + newfile + '"'
+    remove = 'rm "' + originalfile + '"'
+    os.system(command)
+    os.system(remove)
 
+def FindFiles(MainDir):
+    '''
+    Recursively descends into directories until it finds a file that is not
+    a directory. Executes the requisite action, then backs out one directory
+    and moves into the next sub-directory
+    '''
+    for item in Filelist:
+        if os.path.isdir(item):
+            FindFiles(item)
+        else:
+            (Name, Extension) = os.path.splittext(item)
+            if Extension != '.mp3':
+                NewName = Name.strip() + '.mp3'
+                ffmpeg(item, NewName)
+                
+            
+                
+
+
+
+'''
 for item in List:
     if item[-3:] != 'mp3':
         #workingfile = item[:-3]
@@ -18,3 +49,4 @@ for item in List:
         remove = 'rm "' + item + '"'
         os.system(command)
         os.system(remove)
+'''
